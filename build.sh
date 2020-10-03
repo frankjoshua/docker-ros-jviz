66e833d0934a616744a4d6194e8c01d54a7829c9
josh <josh@tesseractmobile.com>
2020-10-02 23:04:41 -0500

    Upgraded build script

--- build.sh
+++ build.sh
@@ -4,17 +4,36 @@ if [[ $? -ne 0 ]]; then
 echo "Docker must have experimental features enabled"
 exit 1
 fi
-# Check for container parameter
-if [[ $# -lt 1 ]]; then
-echo "Usage: build.sh CONTAINER_NAME [--push]"
-exit 1
-fi
+usage() { 
+    echo "Usage: $0 [-t <DOCKER_TAG>] [-q Quiet and Push]" 1>&2; exit 1; 
+}
+while getopts ":t:q" o; do
+    case "${o}" in
+        t)
+            TAG=${OPTARG}
+            ;;
+        q)
+            QUIET_MODE="2> /dev/null"
+            PUSH="--push"
+            ;;
+        :)  
+            echo "ERROR: Option -$OPTARG requires an argument"
+            usage
+            ;;
+        \?)
+            echo "ERROR: Invalid option -$OPTARG"
+            usage
+            ;;
+    esac
+done
+shift $((OPTIND-1))
+
 # Setup qemu for multiplatform builds
-docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
+docker run --rm --privileged multiarch/qemu-user-static --reset -p yes $QUIET_MODE
 # Create builder for docker
 BUILDER=$(docker buildx create --use)
 # Build container on all achitectures in parallel and push to Docker Hub
-docker buildx build -t $1 --platform linux/arm/v7,linux/arm64/v8,linux/amd64 . $2
+eval "docker buildx build $PUSH -t $TAG --platform linux/arm/v7,linux/arm64/v8,linux/amd64 . $QUIET_MODE"
 # Clean up and return error code for CI system if needed
 ERROR_CODE=$?
 docker buildx rm $BUILDER
